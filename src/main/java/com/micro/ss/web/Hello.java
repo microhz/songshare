b02468a8a3f99e5012305c9a0a7d420dbb323253
micro <pengmi2@126.com>
2017-07-01 14:54:00 +0800

    init ss

---
+++ src/main/java/com/micro/ss/web/Hello.java
@@ -0,0 +1,11 @@
+package com.micro.ss.web;
+/**
+ * @author mapc 
+ * @date 2017年7月1日
+ */
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("hello song share !");
+	}
+}
