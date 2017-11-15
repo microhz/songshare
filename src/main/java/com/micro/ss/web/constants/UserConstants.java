cc267b8ee8a2b67a9e6243375555980e6f71ee66
micro <peichuan.mpc@alibaba-inc.com>
2017-11-15 11:17:34 +0800

    fix

--- src/main/java/com/micro/ss/web/constants/UserConstants.java
+++
@@ -1,9 +0,0 @@
-package com.micro.ss.web.constants;
-/**
- * @author mapc 
- * @date 2017年7月1日
- */
-public final class UserConstants {
-
-	public static final String CURRENT_USER_KEY = "currentUser";
-}
